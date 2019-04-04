3f70c4e4afa0fde8b044df8fb03a86e7a8ba4532
Kate Lee <cminlee9@gmail.com>
2019-04-04 11:51:36 +0800

    Rename views to templates and delete html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,39 @@
 const path = require('path')
 const express = require('express')
 
-
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Hello express!</h1>')
+   res.render('index', {
+       title: 'Weather App',
+       name: 'Kate Lee'
+   })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Kate Lee'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is a help page.'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
