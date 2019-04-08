8182c0e5254102ce6f49387170a84d5bc21ad3ab
Kate Lee <cminlee9@gmail.com>
2019-04-08 22:45:16 +0800

    a commit

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -47,7 +47,7 @@ app.get('/weather', (req, res) => {
             error: 'Please provide an address'
         })
     } 
-        geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
             if (error) {
                 return res.send({ error })
             }
@@ -65,8 +65,6 @@ app.get('/weather', (req, res) => {
             })
         })
     
-
-
 })
 
 // 'Cannot set header' error occurs when two responses are sent 
